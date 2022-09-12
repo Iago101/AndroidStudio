6e5fa2d3379022f1254a3afc19ba59563bbb3486
fernandoduarte75 <fernandoduarte@ifsp.edu.br>
2022-09-12 10:45:34 -0300

    Aula 12/09

---
+++ IFitnessApp/app/src/main/java/br/edu/ifsp/arq/ads/dmos5/ifitnessapp/AtividadeCadastroActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.arq.ads.dmos5.ifitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AtividadeCadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atividade_cadastro);
+    }
+}
